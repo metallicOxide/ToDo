e0ce40ac101e448145e045f2bebd09efa55f214a
Jerry <jerrylu1987@hotmail.com>
2020-01-03 15:45:44 +1100

    Updates

---
+++ precache-manifest.3aa89afbba4842aa28b90baaf4134bce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d73d64523fe3bf44b0a444a42eb3479",
+    "url": "/ToDo/index.html"
+  },
+  {
+    "revision": "0fbc1665183a416837d8",
+    "url": "/ToDo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "647f7825b19a6f194dd6",
+    "url": "/ToDo/static/js/2.c380ab67.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/ToDo/static/js/2.c380ab67.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0fbc1665183a416837d8",
+    "url": "/ToDo/static/js/main.cb31857c.chunk.js"
+  },
+  {
+    "revision": "96de4f9d0be23f208a93",
+    "url": "/ToDo/static/js/runtime-main.fc11d095.js"
+  }
+]);
